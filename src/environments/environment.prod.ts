d04ae127265906b656e667b5834ead758b264b11
Akos <akosegypro@gmail.com>
2021-11-08 21:46:36 +0100

    :heavy_plus_sign: Add production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    domain: 'dev--ihngka6.eu.auth0.com',
+    clientId: 'YExphUsxSZurW3NGyT1X5qvj5YRcFymi',
+    audience: 'https://sales-api-dev.com',
+    // httpInterceptor: { // for REST, when using the inerceptor
+    //   allowedList: ['http://localhost:3000/*'],
+    // },
+    redirectUri: 'https://saleswezlangular.azurewebsites.net/login-callback', // Redirect here to handle successful login
+  },
+  graphqlServer: 'https://saleswezl.azurewebsites.net/graphql',
+  appUrl: 'https://saleswezlangular.azurewebsites.net',
 };
